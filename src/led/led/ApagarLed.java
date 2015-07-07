50d927f74ede5a1c8c0ab19fa582838ddcff04a8
dvader33 <vhader33.dr@gmail.com>
2015-07-06 21:20:32 -0600

    definiendo instrucciones de comandos

--- src/led/led/ApagarLed.java
+++ src/led/led/ApagarLed.java
@@ -4,11 +4,12 @@ package led.led;
 public class ApagarLed extends Comando{
 
   public void Ejecutar() {
-      
+      this.led.apagar();
   }
 
   @Override
   public void SetLed(Led led) {
+      this.led=led;
   }
 
 }
--- src/led/led/EncederLed.java
+++ src/led/led/EncederLed.java
@@ -4,11 +4,12 @@ package led.led;
 public class EncederLed extends Comando{
 
   public void Ejecutar() {
+      this.led.Encender();
   }
 
   @Override
   public void SetLed(Led led ) {
-      
+      this.led=led;
   }
 
 }
